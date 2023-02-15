cd2d8c0511bc1143670fe7b5498035e2ee7f097b
unknown <grosheff.ivan@gmail.com>
2023-02-15 10:51:28 +0200

    first commit

--- bot.mjs
+++ bot.mjs
@@ -2,6 +2,11 @@ import TeleBot from "telebot"
 
 const bot = new TeleBot(process.env.TELEGRAM_BOT_TOKEN)
 
+bot.on(['/start', '/hellp'], async (msg) => {
+    await msg.reply.photo('https://www.freeiconspng.com/uploads/black-cat-png-13.png')
+    await msg.reply.text('Добро пожаловать, отправьте свою геопозицию для получения информации о погоде')
+});
+
 bot.on('text', msg => msg.reply.text(msg.text))
 
 export default bot
